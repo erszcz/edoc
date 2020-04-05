229d8116526ad5b6436abe23c691b0abcf442cff
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2020-04-05 10:32:54 +0200

    [skip ci] Save snippets

--- snippets.erl
+++ snippets.erl
@@ -1,6 +1,6 @@
 {ok, {_, [{_, BDocs}]}} = beam_lib:chunks(code:which(edoc), ["Docs"]).
-Docs = erlang:binary_to_term(BDocs).                                     
-rr(edoc_chunks).           
+Docs = erlang:binary_to_term(BDocs).
+rr(edoc_chunks).
 Entries = Docs#docs_v1.docs.
 [ E || {{type, _, _}, _, _, _, _} = E <- Entries ].
 
@@ -17,6 +17,65 @@ Entries = Docs#docs_v1.docs.
 %% > % {{:type, :filename, 0}, 0, ["filename/0"], :none, %{}}
 
 {ok, {_, [{_, BDbgi}]}} = beam_lib:chunks(code:which(edoc), ["Dbgi"]).
-Dbgi = erlang:binary_to_term(BDbgi).                                
+Dbgi = erlang:binary_to_term(BDbgi).
 {_, _, {AST, _}} = Dbgi = erlang:binary_to_term(BDbgi).
-[ Form || {attribute, _, _, _} = Form <- AST ].   
+[ Form || {attribute, _, _, _} = Form <- AST ].
+
+{ok, BChunk} = file:read_file("doctest/chunks/edoc.chunk").
+Chunk = binary_to_term(BChunk).
+
+f().
+edoc:files(["src/edoc.erl"], [{doclet, edoc_doclet_chunks}, {dir, "doctest"}, {layout, edoc_layout_chunk_htmltree}]).
+{ok, BChunk} = file:read_file("_build/default/lib/edoc/doc/chunks/edoc_cli.chunk").
+Chunk = binary_to_term(BChunk).
+
+f().
+edoc:files(["src/edoc.erl"], [{doclet, edoc_doclet_chunks}, {dir, "doctest"},
+			      {layout, edoc_layout_chunk_htmltree}]).
+{ok, BChunk} = file:read_file("doctest/chunks/edoc.chunk").
+Chunk = binary_to_term(BChunk).
+
+f(ReadChunk).
+ReadChunk = fun (File) ->
+                    {ok, BChunk} = file:read_file(File),
+                    Chunk = binary_to_term(BChunk)
+            end.
+ReadChunk("doctest/recon-chunks/chunks/recon.chunk").
+ReadChunk("doctest/recon-markdown-chunks/chunks/recon.chunk").
+ReadChunk("doctest/stdlib-chunks/chunks/ets.chunk").
+ReadChunk("/Users/erszcz/apps/erlang/shell_docs_dfa2189/lib/lib/stdlib-3.11.2/doc/chunks/shell_docs.chunk").
+
+%% This fails on missing logger.hrl...
+edoc:application(recon, [{doclet, edoc_doclet_chunks},
+                         {layout, edoc_layout_chunk_htmltree},
+                         {preprocess, true},
+                         {includes, ["/Users/erszcz/.asdf/installs/erlang/21.1/lib/kernel-6.1/include",
+                                     "/Users/erszcz/.asdf/installs/erlang/21.1/lib/stdlib-3.6/include/"]},
+                         {dir, "doctest/recon-chunks"}]).
+
+%% ...but this fails on missing logger.hrl, too,
+%% so it's not a regression.
+%% Simply, edoc:application() doesn't handle `include` paths
+%% based on `.app` contents.
+edoc:application(stdlib, [{preprocess, true},
+                          {includes, ["/Users/erszcz/.asdf/installs/erlang/21.1/lib/kernel-6.1/include"]},
+                          {dir, "doctest/stdlib-default"}]).
+
+edoc:application(recon, [{doclet, edoc_doclet_chunks},
+                         {layout, edoc_layout_chunk_markdown},
+                         {preprocess, true},
+                         {includes, ["/Users/erszcz/.asdf/installs/erlang/21.1/lib/kernel-6.1/include",
+                                     "/Users/erszcz/.asdf/installs/erlang/21.1/lib/stdlib-3.6/include/"]},
+                         {dir, "doctest/recon-markdown-chunks"}]).
+
+dbg:stop_clear().
+dbg:tracer().
+dbg:p(all, call).
+dbg:tpl(edoc_layout_chunks, edoc_to_chunk, x).
+%dbg:tpl(edoc_extract, collect, x).
+%dbg:tpl(edoc_layout_chunks, edoc_extract_function, x).
+%dbg:tpl(edoc, run, x).
+%dbg:tpl(edoc_doclet_default, source, []).
+%dbg:tpl(edoc_doclet_chunks, options_with_defaults, x).
+%dbg:tpl(edoc_lib, run_layout, x).
+%dbg:tpl(edoc_lib, run_plugin, x).
