7c577b1bbc3904311b9769f75806644b4c2893f0
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2020-08-20 12:22:45 +0200

    [skip ci] Sketch how EDoc links should map to OTP see* link types

--- test/eep48_SUITE_data/eep48_links.erl
+++ test/eep48_SUITE_data/eep48_links.erl
@@ -1,3 +1,6 @@
+%% @doc Fixtures for chunk links.
+%% See http://erlang.org/doc/apps/erl_docgen/inline_tags.html#%3Csee*%3E---see-tags for
+%% the description of `seemfa', `seetype', etc link types.
 -module(eep48_links).
 
 -export([f/0,
@@ -17,25 +20,33 @@
 f() -> ok.
 
 %% @doc Link to this module {@link eep48_links}.
+%% Should map to `seeerl'.
 module_link() -> ok.
 
 %% @doc Link to application {@link //edoc}.
+%% Should map to `seeapp'.
 app_link() -> ok.
 
 %% @doc Link to application module {@link //edoc/edoc_doclet}.
+%% Should map to `seeerl'.
 app_module_link() -> ok.
 
 %% @doc Link to application M:F/A {@link //edoc/edoc:files/2}.
+%% Should map to `seemfa'.
 app_mfa_link() -> ok.
 
 %% @doc Link to external function {@link eep48_SUITE:suite/0}.
+%% Should map to `seemfa'.
 external_function_link() -> ok.
 
 %% @doc Link to local function {@link f/0}.
+%% Should map to `seemfa'.
 local_function_link() -> ok.
 
 %% @doc Local type link {@link t()}.
+%% Should map to `seetype'.
 local_type_link() -> ok.
 
 %% @doc External type link {@link eep48_links:t()}.
+%% Should map to `seetype'.
 external_type_link() -> ok.
